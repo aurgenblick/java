9e4118300623387051754322876f39936e171051
aurgenblick <aurgenblick@gmail.com>
2022-06-21 19:09:24 +0300

    ignored errors

---
+++ src/org/itmo/java/lesson2/Calculator.java
@@ -0,0 +1,42 @@
+package org.itmo.java.lesson2;
+
+public class Calculator {
+
+    void sum(double number1, double number2) {
+        sum = number1+number2;
+    }
+    void sum(long number1, long number2) {
+        sum = number1+number2;
+    }
+    void sum(int number1, int number2) {
+        sum = number1+number2;
+    }
+    void div(double number1, double number2) {
+        div = number1/number2;
+    }
+    void div(long number1, long number2) {
+        div = number1/number2;
+    }
+    void div(int number1, int number2) {
+        div = number1/number2;
+    }
+    void mult(double number1, double number2) {
+        mult = number1*number2;
+    }
+    void mult(long number1, long number2) {
+        mult = number1*number2;
+    }
+    void mult(int number1, int number2) {
+        mult = number1*number2;
+    }
+    void subt(double number1, double number2) {
+        subt = number1-number2;
+    }
+    void subt(long number1, long number2) {
+        subt = number1-number2;
+    }
+    void subt(int number1, int number2) {
+        subt = number1-number2;
+    }
+
+}
