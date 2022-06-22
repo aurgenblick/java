f78920cfd5a3da80467dc850fafcf042e2dde9ce
aurgenblick <aurgenblick@gmail.com>
2022-06-22 12:26:21 +0300

    refactored packages structure

---
+++ src/org/itmo/java/labs/task3/ex1/Study.java
@@ -0,0 +1,24 @@
+package org.itmo.java.labs.task3.ex1;
+
+class Study {
+
+    private String course;
+
+    //В классе Study дописан конструктор, который принимает один параметр (String с) м устанавливает это значение для поля "course".
+    Study(String c) {
+        course = c;
+    }
+
+    public String printCourse() {
+        return this.course;
+    }
+}
+
+class JavaProgram {
+    public static void main(String[] args) {
+        //Создание объекта класса Study с заданным значением поля
+        Study Study1 = new Study("Изучение Java - это просто!");
+        //Обращение к методу printCourse для вывода значения поля course
+        System.out.println(Study1.printCourse());
+    }
+}
