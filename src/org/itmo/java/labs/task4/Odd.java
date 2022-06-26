a03f31ebf740cdcc254f5008595164c747eac98f
aurgenblick <aurgenblick@gmail.com>
2022-06-26 17:25:36 +0300

    update to task3 (exercise 4 finished)

---
+++ src/org/itmo/java/labs/task4/Odd.java
@@ -0,0 +1,17 @@
+package org.itmo.java.labs.task4;
+
+import org.jetbrains.annotations.NotNull;
+
+class Odd {
+    //
+    private Integer Numbers [100];
+
+    public Integer void setNumbers(Integer x) {
+        Numbers[x] = x+1;
+    }
+
+
+    public static main (String[] args){
+
+    }
+}
