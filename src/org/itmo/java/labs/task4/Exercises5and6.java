2648720f19835b367154b8b1d8e3b81c84dc001c
aurgenblick <aurgenblick@gmail.com>
2022-06-27 21:08:42 +0300

    Added exercise 6 to part One

---
+++ src/org/itmo/java/labs/task4/Exercises5and6.java
@@ -0,0 +1,33 @@
+package org.itmo.java.labs.task4;
+
+import java.util.Scanner;
+
+import static java.lang.System.*;
+
+public class Exercises5and6 {
+    public static Integer[] InputNumbers = new Integer[7];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(in);
+        out.println("Введите семь чисел:");
+        for (int i = 0; i < InputNumbers.length; i++) {
+            InputNumbers[i] = Integer.parseInt(sc.next());
+        }
+
+        out.println("array: " + InputNumbers);
+        //Переменной присваивается true, если первое или последнее введённое число равно 3
+        Boolean isThree = (InputNumbers[0] == 3 | InputNumbers[InputNumbers.length - 1] == 3) ? true : false;
+        out.println(isThree);
+
+        //Переменной присваивается true, если массив содержит число 1 или 3
+        Boolean isOneOrThree = false;
+        for (Integer i : InputNumbers) {
+            if (InputNumbers[i] == 1 | InputNumbers[i] == 3) ;
+            {
+                isOneOrThree = true;
+                break;
+            }
+        }
+        out.println(isOneOrThree);
+    }
+}
